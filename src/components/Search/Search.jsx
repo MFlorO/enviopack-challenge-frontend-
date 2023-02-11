70232c57061c4d4b9ba51e19e226787ea9bf5b61
Flori O <floroldani@hotmail.com>
2023-02-11 20:04:55 -0300

    Filtar por caracteres

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -8,40 +8,14 @@ const Search = () => {
 
   const [search, setSearch] = useState("")
 
+  const onSearchChange = (event) => {
+    setSearch((event.target.value).toLowerCase())
+    setProductCopia(productState.filter( f => f.title.toLowerCase().includes( search.toLowerCase() )))
+  }
 
-  const onSearchChange = (event) => setSearch((event.target.value).toUpperCase())
-
-
-  const filtrar = (search) => {
-
-    const funcion = (e) =>  e.title.toString().toUpperCase().includes(search)
-
-    if (productCopia && productCopia.length === productState.length){  //Caso: Busco por primera vez
-
-      const resultados = productCopia?.filter( (e) => funcion(e)) 
-
-      resultados.length > 0 ? setProductCopia(resultados) :  setProductCopia("")  //Busco y NO hay coincidencias --> Esto utilizo para el boton de TRY AGAIN
-                                 //Busco y hay concidencias: Muestro la copia con los resultados 
-
-    } else {  //Caso: Busco por segunda vez o más
-
-      const resultados = productState.filter( (e) => funcion(e)) //Filtro el original
-      
-      resultados.length > 0 ? setProductCopia(resultados) :  setProductCopia("")
-    }
-  };
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); 
-	  filtrar(search.trim());
-    setSearch("");
-  };
-
-
+  
   return (
-    
-    <form onSubmit={handleSubmit} className={styles.conteiner}>
+    <div className={styles.conteiner}>
       <input 
         type="text"
         placeholder="Buscar productos por nombre "
@@ -50,7 +24,7 @@ const Search = () => {
         disabled={!productCopia} 
       />
       
-    </form>
+    </div>
   )
 };
 
