90279cf8bcafeef099129123df627af05b45bd47
Flori O <floroldani@hotmail.com>
2023-02-12 21:57:09 -0300

    disabled de paginado

--- src/components/Paginated/Paginated.jsx
+++ src/components/Paginated/Paginated.jsx
@@ -5,16 +5,21 @@ import styles from "./Paginated.module.css"
 
 const Paginated = () => {
 
-  const [, , , , , , , currentPage, setCurrentPage, itemsPerPage] = useProductContext();
+  const [, , , , filterProducts, , , currentPage, setCurrentPage, itemsPerPage] = useProductContext();
 
   const previuos = () => currentPage > 0 && setCurrentPage(currentPage - itemsPerPage);
 
   const next = () => setCurrentPage(currentPage + itemsPerPage);
 
+  const page = currentPage === 0 ? currentPage + 1 : (currentPage / itemsPerPage) + 1
+
+	const disabledPreviuos = page !== 1
+  const disabledNext = filterProducts().length < itemsPerPage
+  
   return (
     <div className={styles.conteiner}>
-      <button onClick={previuos}><GrFormPrevious /></button>
-      <button onClick={next}><GrFormNext /></button>
+      <button onClick={previuos} disabled={!disabledPreviuos}><GrFormPrevious /></button>
+      <button onClick={next} disabled={disabledNext}><GrFormNext /></button>
     </div>
   );
 };
